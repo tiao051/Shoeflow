b2ba8e2df25cadc2b1bf7584300e4a2a810350aa
tiao051 <nguyenminhtho0503@gmail.com>
2025-11-24 18:33:40 +0700

    feat: add chatbox icon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -114,6 +114,18 @@
         <a href="#" class="block px-4 py-2 hover:bg-gray-100 sale text-red-600">Sale Up To 50%</a>
         <a href="#" class="block px-4 py-2 hover:bg-gray-100 border-t mt-2">Sign In</a>
     </div>
+    <a href="https://m.me/conversevn" target="_blank" 
+    class="fixed bottom-6 right-6 z-50 bg-black text-white p-3 rounded-full shadow-2xl hover:bg-red-600 transition duration-300 transform hover:scale-110"
+    title="Chat with us via Facebook Messenger">
+        <svg xmlns="http://www.w3.org/2000/svg" 
+            class="h-8 w-8" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            stroke="currentColor" 
+            stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 4v-4z" />
+        </svg> 
+    </a>
 </header>
 <script>
 const BASE_URL = '{{ asset('/') }}';
